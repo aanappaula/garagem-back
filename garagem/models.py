aaa740c88d772fb167dd72afe4b7f11d78cae6f9
Ana Paula <aninha10.desouza@gmail.com>
2023-04-25 15:01:35 -0300

    Criação dos endpoints da categoria

--- garagem/models.py
+++ garagem/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Marca(models.Model):
     nome = models.CharField(max_length=50)
     nacionalidade = models.CharField(max_length=50, null=True, blank=True)
@@ -7,11 +8,13 @@ class Marca(models.Model):
     def __str__(self):
         return f"{self.nome.upper()}"
 
+
 class Categoria(models.Model):
     descricao = models.CharField(max_length=100)
 
     def __str__(self):
         return f"{self.descricao} ({self.id})"
+
 
 class Acessorio(models.Model):
     descricao = models.CharField(max_length=100)
@@ -22,7 +25,8 @@ class Acessorio(models.Model):
     class Meta:
         verbose_name = "acessório"
         verbose_name_plural = "acessórios"
-    
+ 
+   
 class Cor(models.Model):
     descricao = models.CharField(max_length=100)
 
@@ -32,7 +36,8 @@ class Cor(models.Model):
     class Meta:
         verbose_name = "cor"
         verbose_name_plural = "cores"
-    
+
+   
 class Veiculo(models.Model):
     marca = models.ForeignKey(
         Marca, on_delete=models.PROTECT, related_name="veiculos"
--- garagem/serializers.py
+++ garagem/serializers.py
@@ -1,9 +1,17 @@
 from rest_framework.serializers import ModelSerializer
 
-from garagem.models import Marca
+from garagem.models import Marca, Categoria
 
 
 class MarcaSerializer(ModelSerializer):
     class Meta:
         model = Marca
         fields = "__all__"
+        
+
+class CategoriaSerializer(ModelSerializer):
+    class Meta:
+        model = Categoria
+        fields = "__all__"
+        
+        
