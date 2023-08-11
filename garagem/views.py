0560ff0694fd90dd52bc1a42e5995967436cee82
Ana <aninha10.desouza@gmail.com>
2023-08-11 20:25:19 -0300

    Aula 12 - Permissões do DRF

--- garagem/views.py
+++ garagem/views.py
@@ -1,27 +1,36 @@
 from rest_framework.viewsets import ModelViewSet
 
+from rest_framework.permissions import IsAuthenticated
+
 from garagem.models import Marca, Categoria, Cor, Acessorio, Modelo, Veiculo
 from garagem.serializers import MarcaSerializer, CategoriaSerializer, CorSerializer, AcessorioSerializer, ModeloSerializer, VeiculoSerializer, VeiculoDetailSerializer, VeiculoListSerializer
+
 
 class MarcaViewSet(ModelViewSet):
     queryset = Marca.objects.all()
     serializer_class = MarcaSerializer
 
+
 class CategoriaViewSet(ModelViewSet):
     queryset = Categoria.objects.all()
     serializer_class = CategoriaSerializer
+    permission_classes = [IsAuthenticated]
+
 
 class CorViewSet(ModelViewSet):
     queryset = Cor.objects.all()
     serializer_class = CorSerializer
 
+
 class AcessorioViewSet(ModelViewSet):
     queryset = Acessorio.objects.all()
     serializer_class = AcessorioSerializer
 
+
 class ModeloViewSet(ModelViewSet):
     queryset = Modelo.objects.all()
     serializer_class = ModeloSerializer
+
 
 class VeiculoViewSet(ModelViewSet):
     queryset = Veiculo.objects.all()
